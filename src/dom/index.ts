5194c7ad7edad1e2ea5529864640560f9661a371
Ali Sabil <ali.sabil@gmail.com>
2018-03-04 20:02:20 +0100

    Add workaround for h function typing

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,2 +1,17 @@
-export { h } from "snabbdom";
-export { VNode } from "snabbdom/vnode";
+import { h as h0 } from "snabbdom";
+import { VNode, VNodeData } from "snabbdom/vnode";
+
+export { VNode, VNodeData };
+export type VNodes = ReadonlyArray<VNode>;
+export type VNodeChildElement = VNode | string | number | undefined | null;
+export type ArrayOrElement<T> = T | ReadonlyArray<T>;
+export type VNodeChildren = ArrayOrElement<VNodeChildElement>;
+
+// tslint:disable:unified-signatures
+export function h(sel: string): VNode;
+export function h(sel: string, data: VNodeData): VNode;
+export function h(sel: string, children: VNodeChildren): VNode;
+export function h(sel: string, data: VNodeData, children: VNodeChildren): VNode;
+export function h(sel: any, b?: any, c?: any): VNode {
+  return h0(sel, b, c);
+}
